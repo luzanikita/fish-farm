33d0b58d7dc7a90f31e69b6005bd1989ca320512
Nikita Luzan <luzanikita@gmail.com>
2020-04-21 00:59:23 +0300

    Added basic db scheme.

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	_ "github.com/nigi4/fish-farm/routers"
+
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
 	beego.Run()
 }
 
--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,32 @@
+// @APIVersion 1.0.0
+// @Title beego Test API
+// @Description beego has a very cool tools to autogenerate documents for your API
+// @Contact astaxie@gmail.com
+// @TermsOfServiceUrl http://beego.me/
+// @License Apache 2.0
+// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
 package routers
 
 import (
 	"github.com/nigi4/fish-farm/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	ns := beego.NewNamespace("/v1",
+
+		beego.NSNamespace("/users",
+			beego.NSInclude(
+				&controllers.UsersController{},
+			),
+		),
+
+		beego.NSNamespace("/farms",
+			beego.NSInclude(
+				&controllers.FarmsController{},
+			),
+		),
+	)
+	beego.AddNamespace(ns)
 }
