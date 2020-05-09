2cbe7c37dba9dcf6471f7fd4dc084cb3c8a3e24a
Nikita Luzan <luzanikita@gmail.com>
2020-05-09 15:41:34 +0300

    Cors enabled.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/nigi4/fish-farm/routers"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/lib/pq"
 )
@@ -14,6 +15,10 @@ func main() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowCredentials: true,
+	}))
 	beego.Run()
 }
 
